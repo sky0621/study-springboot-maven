cf9473c1b0dd55b828bee5d3d52970b5e362a92a
S.S <ss@ss.com>
2016-05-17 23:49:50 +0900

    05/17 Retry. Chap3.2 PathVariable

--- MyBootApp/src/main/java/xyz/skycat/mvn/springboot/IndexController.java
+++ MyBootApp/src/main/java/xyz/skycat/mvn/springboot/IndexController.java
@@ -1,13 +1,18 @@
 package xyz.skycat.mvn.springboot;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
 
-	@RequestMapping("/")
-	public String index() {
-		return "Hello Spring-Boot World!";
+	@RequestMapping("/{num}")
+	public String index(@PathVariable int num) {
+		int res = 0;
+		for (int i = 0; i < num; i++) {
+			res += i;
+		}
+		return "total: " + res;
 	}
 }
