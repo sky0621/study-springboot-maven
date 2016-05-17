df710ad3bdf377accae4a93c633a7a1fa79b80a6
S.S <ss@ss.com>
2016-05-17 23:58:13 +0900

    05/17 Retry. Chap3.3

--- MyBootApp/src/main/java/xyz/skycat/mvn/springboot/IndexController.java
+++ MyBootApp/src/main/java/xyz/skycat/mvn/springboot/IndexController.java
@@ -1,18 +1,13 @@
 package xyz.skycat.mvn.springboot;
 
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class IndexController {
 
-	@RequestMapping("/{num}")
-	public String index(@PathVariable int num) {
-		int res = 0;
-		for (int i = 0; i < num; i++) {
-			res += i;
-		}
-		return "total: " + res;
+	@RequestMapping("/")
+	public String index() {
+		return "index";
 	}
 }
