951d39d53eede3507ec3ab980d984d62f77e16d2
S.S <ss@ss.com>
2016-05-18 00:04:30 +0900

    05/17 Retry. Chap3.3 Model

--- MyBootApp/src/main/java/xyz/skycat/mvn/springboot/IndexController.java
+++ MyBootApp/src/main/java/xyz/skycat/mvn/springboot/IndexController.java
@@ -1,13 +1,16 @@
 package xyz.skycat.mvn.springboot;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
-	@RequestMapping("/")
-	public String index() {
+	@RequestMapping("/{num}")
+	public String index(@PathVariable int num, Model model) {
+		model.addAttribute("msg", num);
 		return "index";
 	}
 }
